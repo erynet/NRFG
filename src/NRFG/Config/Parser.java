8b618bf8ca24198c4cddf215ce7b4b35ca3c849a
ery <ldh668@gmail.com>
2014-11-09 18:03:43 +0900

    2

--- src/NRFG/Config/Parser.java
+++ src/NRFG/Config/Parser.java
@@ -8,8 +8,19 @@ package NRFG.Config;
  *
  */
 
+//import org.apache.commons.configuration.ConfigurationException;
+//import org.apache.commons.configuration.XMLConfiguration;
 import org.apache.commons.configuration.*;
 
 public class Parser {
+	public Parser(String[] args) {
 
+			//XMLConfiguration config = new XMLConfiguration("NRFG.xml");
+			Parameters params = new Parameters();
+			FileBasedConfigurationBuilder<XMLConfiguration> builder = 
+					new FileBasedConfigurationBuilder<XMLConfiguration>(XMLConfiguration.class)
+					.configure(params.xml())
+						.setFileName("config.xml");
+			XMLConfiguration config = builder.getConfiguration();
+	}
 }
